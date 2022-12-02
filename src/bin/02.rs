a185896a607143bb3c6c290a9b0523b00cf015ec
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-12-02 19:47:29 +0100

    Day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,9 +1,23 @@
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i32> {
+    Some(
+        input
+            .as_bytes()
+            .split(|b| *b == b'\n')
+            .map(|e| ((e[0] - b'A') as i32, (e[2] - b'X') as i32))
+            .map(|(a, b)| 1 + b + 3 * (1 + b - a).rem_euclid(3))
+            .sum::<i32>(),
+    )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    Some(
+        input
+            .as_bytes()
+            .split(|b| *b == b'\n')
+            .map(|e| ((e[0] - b'A') as i32, (e[2] - b'X') as i32))
+            .map(|(a, b)| 1 + b * 3 + (a + b + 2) % 3)
+            .sum::<i32>(),
+    )
 }
 
 fn main() {
@@ -19,12 +33,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(15));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
